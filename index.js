fa6118547693116d3f903f793e4465a65ae10d7d
Roman Garanin <roman_garanin@epam.com>
2022-06-10 12:35:27 +0000

    Get and Post routes have been added

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
 const SmartPhone = require('./models/smartphone.model');
 const express = require("express");
 const app = express();
+const router = require("./routes/smphns");
 
 function main() {
 
     const PORT = process.env.PORT || 3000;
-    
-    app.get("/", (req, res) => {
-        res.send("Hello world!");
-    });
-
-    app.get("/smphns", (req, res) => {
-        res.send("smphn1; smphn2; smphn3;");
-    });
-
+    app.use("/smphns", router);
     app.listen(PORT, () => {
         console.log("ExpressJS is running!");
     })
