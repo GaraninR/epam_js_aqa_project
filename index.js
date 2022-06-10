e323edc9c3f571b83cfb47025e9dfaea4edcf2b3
Roman Garanin <roman_garanin@epam.com>
2022-06-10 12:10:14 +0000

    Todo

--- index.js
+++ index.js
@@ -4,16 +4,20 @@ const app = express();
 
 function main() {
 
+    // TODO: get port number from environment variable
+    // FIXME: change port immediately
     const PORT = process.env.PORT || 3000;
     
     app.get("/", (req, res) => {
         res.send("Hello world!");
     });
 
+    // TODO: Select smartphones from database
     app.get("/smphns", (req, res) => {
         res.send("smphn1; smphn2; smphn3;");
     });
 
+    // TODO: Move log message to confing
     app.listen(PORT, () => {
         console.log("ExpressJS is running!");
     })
