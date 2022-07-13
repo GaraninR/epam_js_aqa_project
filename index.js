6df0c46a433925d44760244ff66671f402fb3f8b
Roman Garanin <roman_garanin@epam.com>
2022-07-13 13:09:27 +0000

    Getting changes from tmp branch

--- index.js
+++ index.js
@@ -1,23 +1,20 @@
 const SmartPhone = require('./models/smartphone.model');
 const express = require("express");
 const app = express();
+app.use(express.json());
+const router = require("./routes/smrtphs");
+const routerFS = require("./routes/filesystem")
+const routerDB = require("./routes/databases")
 
 function main() {
 
     // TODO: get port number from environment variable
     // FIXME: change port immediately
     const PORT = process.env.PORT || 3000;
-    
-    app.get("/", (req, res) => {
-        res.send("Hello world!");
-    });
+    app.use("/smphns", router);
+    app.use("/fs", routerFS);
+    app.use("/db", routerDB);
 
-    // TODO: Select smartphones from database
-    app.get("/smphns", (req, res) => {
-        res.send("smphn1; smphn2; smphn3;");
-    });
-
-    // TODO: Move log message to confing
     app.listen(PORT, () => {
         console.log("ExpressJS is running!");
     })
